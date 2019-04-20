560858fc80dfc5251b621caad3897c0e4039a540
kncoom <kncoom9174@gmail.com>
2019-04-20 17:56:57 +0900

    Flag 페이지

---
+++ flag.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="flag.css">
+    <title>flag!!!</title>
+</head>
+<body>
+	<div class="flag_box">
+    <p>flag is {h@nSei_ls_@_g0od_h!gh_sChoOI}</p>
+    </div>
+</body>
+</html>
